424c428cb68c6dc2b3c8b8e48123ceb758650c31
Jimmy <yt25707321@gmail.com>
2022-11-26 15:29:58 +0800

    Add order system

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,23 @@
+import { NavBar } from "../components/NavBar"
+import { List } from "../components/Table"
+
+interface ListProps {
+  food: string;
+  price: number;
+}
+
 export const HomePage = () => {
+  const Order: ListProps[] = [
+    { food: '土魠魚羹', price: 20000 },
+    { food: '土魠魚羹', price: 20000 },
+    { food: '土魠魚羹', price: 20000 },
+    { food: '土魠魚羹', price: 20000 },
+  ]
+
   return (
     <>
-      <div>Home</div>
+      <div><NavBar /></div>
+      <div><List Order={Order} /></div>
     </>
   )
 }
