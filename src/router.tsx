81f119b6137cea1d3a3f9bf213afe64b7342de0f
Jimmy <yt25707321@gmail.com>
2022-11-26 16:02:58 +0800

    fix conflix

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,17 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { Transaction_History } from "./pages/Transaction_History";
 
 export const useRouter = () => {
-  return createBrowserRouter([{
-    path: '/',
-    element: <HomePage />
-  }]);
+  return createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/history',
+      element: <Transaction_History />,
+    }
+  ]);
 }
