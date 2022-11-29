33b563e0e4725a5810c37e6565730f12df21c04c
Jimmy <yt25707321@gmail.com>
2022-11-29 17:13:36 +0800

    Data integration

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,39 @@
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { SimpleGrid, Card } from "@chakra-ui/react";
+import { CardsContent } from "./CardContent";
+
+interface cardProps {
+  title: string;
+  message: string;
+  src: string;
+}
+
+interface cardsProps {
+  cards: cardProps[];
+}
+
+export const Cards = ({ cards }: cardsProps) => {
+  const bgc = "#E2D6C8";
+  return (
+    <>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
+        <Card backgroundColor={bgc}>
+          {cards.map((card) => {
+            return (
+              <>
+                <CardsContent
+                  title={card.title}
+                  message={card.message}
+                  src={card.src}
+                ></CardsContent>
+              </>
+            );
+          })}
+        </Card>
+      </SimpleGrid>
+    </>
+  );
+};
